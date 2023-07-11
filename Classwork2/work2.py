8abf2c602477dc530df873a2e972f133f4220363
Jirachai <77221588+JaoTaohu@users.noreply.github.com>
2023-07-11 20:14:54 +0700

    finish classwork2

--- Classwork2/work2.py
+++ Classwork2/work2.py
@@ -1,14 +1,20 @@
 import numpy as np
-import cv2
+import cv2 as cv
 
 # Load the image
-image_path = "image/cc.png"  # Replace with the path to your image
-image = cv2.imread(image_path)
+image = cv.imread('image/cc.png')
+gray = cv.cvtColor(image, cv.IMREAD_GRAYSCALE)
+blur = cv.medianBlur(gray, 5)
+edges = cv.Canny(blur,230,255)
 
-# Reverse the color
-reversed_image = 255 - image
+for row in range(0,image.shape[0],3):
+    for col in range(0,image.shape[1],3):
+        pixel = edges[row,col]
+        if pixel > 230:
+            radius = 60
+            cv.circle(image, (col,row), radius,  (0,255,0),1)
 
-# Display the reversed image
-cv2.imshow("Reversed Image", reversed_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+cv.imshow("Circle", image)
+cv.waitKey(0)
+cv.destroyAllWindows()

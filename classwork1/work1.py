6176bf974aace893bf48e5ba0a937e852263e6cf
Jirachai <77221588+JaoTaohu@users.noreply.github.com>
2023-07-16 00:58:27 +0700

    finish classwork1

--- classwork1/work1.py
+++ classwork1/work1.py
@@ -1,26 +1,27 @@
 import numpy as np
-import cv2
+import cv2 as cv
 
-# Create a black image with a white background
-def line():
-    image = np.zeros((500, 500, 3), dtype=np.uint8)
+img = cv.imread('image/shurima.jpg', cv.IMREAD_GRAYSCALE)
+kernel_size = 15
 
-    # Define the start and end points of the line
-    start_point = (50, 50) # (x1, y1)
-    end_point = (400, 400) # (x2, y2)
+# สร้างเมทริกซ์เปล่าที่มีค่าเป็น 0
+kernel = np.zeros((kernel_size, kernel_size), np.float32)
 
-# Calculate the slope and intercept of the line
-    slope = (end_point[1] - start_point[1]) / (end_point[0] - start_point[0])
-    intercept = start_point[1] - slope * start_point[0]
+# กำหนดค่าความชันและค่าคงที่ของสมการเส้นตรง
+m = 1  # ค่าความชัน (slope)
+c = 2  # ค่าคงที่ (intercept)
 
-# Iterate over the x-coordinates of the line and set corresponding pixel values
-    for x in range(start_point[0], end_point[0] + 1):
-        y = int(slope * x + intercept)
-        image[y, x] = (255, 255, 255) #set colour
-    
-    cv2.imwrite("classwork1/Linear Line.png", image)
-    
+# คำนวณและกำหนดค่าในเมทริกซ์ให้เป็น 1 ตามสมการเส้นตรง
+for i in range(kernel_size):
+    j = int(m * i + c)
+    if 0 <= j < kernel_size:
+        kernel[j, i] = 1
 
-    
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# ทำการ normalize kernel โดยหารด้วยผลรวมของค่าใน kernel
+kernel /= np.sum(kernel)
+
+print(kernel)
+
+output = cv.filter2D(img, -1, kernel, borderType=cv.BORDER_REFLECT)
+cv.imwrite('classwork1/input.png', img)
+cv.imwrite('classwork1/output.png', output)
